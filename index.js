1c600d2cd9e86f04367e8aafce6c349589712a29
OlegBarabash <olegbarabash7@gmail.com>
2024-02-20 22:09:28 +0200

    start search

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import puppeteer from "puppeteer";
 
+const searchTermCLI =
+  process.argv.length >= 3 ? process.argv[2] : "macbook pro";
+const url = "https://www.amazon.com/";
+
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-
-  await page.goto("https://developer.chrome.com/");
-
   await page.setViewport({ width: 1280, height: 1024 });
-  await page.screenshot({ path: "./screens/first_screenshot.jpg" });
+
+  await page.goto(url);
+
+  const searchInputSelector = "twotabsearchtextbox";
+  await page.waitForSelector(searchInputSelector);
+  await page.type(searchInputSelector, searchTermCLI, { delay: 100 });
+
+  // const searchResultSelector = ".ng-star-inserted";
+  // await page.waitForSelector(searchResultSelector);
+
+  await page.screenshot({ path: "./screens/macBook_screenshot.jpg" });
 
   browser.close();
 })();
